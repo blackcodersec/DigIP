3a8f08773994fe4292e92596ae2457c15d434110
Akash Sarkar <46501627+0xAkashsec@users.noreply.github.com>
2021-02-05 00:15:51 +0600

    Update digip.sh

--- digip.sh
+++ digip.sh
@@ -8,18 +8,19 @@ echo " | (_| | | (_| | | | |_) |  ";
 echo "  \__,_|_|\__, | |_| .__/   ";
 echo "           __/ |   | |      ";
 echo "          |___/    |_|      ";
+echo "                   @0xakashsec";
 echo -e "\n"
-echo -e "[+] DigIP.sh by Black Coder\n"
+echo -e "[+] Total Domain list [$(cat $1|wc -l)]\n"
 if [ -z "$1" ] || [ -z "$2" ]; then
   echo "[!] Usage: ./digip.sh [domain-list-file] [output-file]"
   exit 1
 fi
 echo "[+] Resolving domains to IPs..."
 while read d || [[ -n $d ]]; do
-  ip=$(dig +short $d|grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b"|head -1)
+  ip=$(dig +short $d | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b")
   if [ -n "$ip" ]; then
-    echo "[+] '$d' => $ip"
-    echo $ip >> $2
+    echo -e "[+] Domain '$d' \n$ip"
+    echo -e "$ip" | tee -a $2 &>/dev/null
   else
     echo "[!] '$d' => [RESOLVE ERROR]"
   fi
@@ -27,4 +28,6 @@ done < $1
 echo -e "\n[+] Removing duplicates..."
 sort $2 | uniq > $2.new
 mv $2.new $2
-echo -e "\n[+] Done.\n===>IPs saved to '$2'."
+echo -e "\n[+] Done."
+echo -e "[+] Total IP Found [$(cat $2|wc -l)]\n"
+echo -e "[+]IPs saved to '$2'."
